6dbaab5df71daffdb2d063f5e7ad1d20ab183a68
kmw10693 <kmw10693@konkuk.ac.kr>
2024-03-05 00:21:58 +0900

    [Feat] 로그인/로그아웃 기능 실습

---
+++ src/main/java/next/web/LoginServlet.java
@@ -0,0 +1,33 @@
+package next.web;
+
+import core.db.DataBase;
+import next.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebServlet("/user/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+
+        User user = DataBase.findUserById(userId);
+
+        HttpSession session = req.getSession();
+        if(user.getPassword().equals(password)) {
+            session.setAttribute("user", user);
+        }
+        resp.sendRedirect("/user/list");
+    }
+}
--- src/main/java/next/web/LogoutServlet.java
+++ src/main/java/next/web/LogoutServlet.java
@@ -3,26 +3,24 @@ package next.web;
 import core.db.DataBase;
 import next.model.User;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebServlet("/user/logout")
 
-@WebServlet("/users/updateForm")
-public class UpdateUserFormServlet extends HttpServlet {
+public class LogoutServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String userId = req.getParameter("userId");
 
-        User user = DataBase.findUserById(userId);
+        HttpSession httpSession = req.getSession();
+        httpSession.invalidate();
+        resp.sendRedirect("/index.jsp");
 
-        req.setAttribute("user", user);
-        RequestDispatcher rd = req.getRequestDispatcher("/user/update.jsp");
-        rd.forward(req, resp);
     }
 }
