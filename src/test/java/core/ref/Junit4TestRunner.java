b03b6be56cd1ece3c070f42e7c44fee2a6e44ca7
kmw10693 <kmw10693@konkuk.ac.kr>
2024-03-10 08:23:03 +0900

    [Feat] 10.1.2 자라 리플렉션 구현

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -2,10 +2,18 @@ package core.ref;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            if(declaredMethod.isAnnotationPresent(MyTest.class)) {
+                declaredMethod.invoke(clazz.newInstance());
+            }
+        }
 
     }
 }
--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -7,6 +7,11 @@ import org.slf4j.LoggerFactory;
 import next.model.Question;
 import next.model.User;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
@@ -14,17 +19,38 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        logger.debug(Arrays.toString(clazz.getDeclaredFields()));
+        logger.debug(Arrays.toString(clazz.getDeclaredConstructors()));
+        logger.debug(Arrays.toString(clazz.getDeclaredMethods()));
     }
     
     @Test
-    public void newInstanceWithConstructorArgs() {
+    public void newInstanceWithConstructorArgs() throws InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<User> clazz = User.class;
         logger.debug(clazz.getName());
+
+        Object o = null;
+        Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+        for (Constructor<?> declaredConstructor : declaredConstructors) {
+           o = declaredConstructor.newInstance("a", "b", "c", "d");
+        }
+        System.out.println(o);
     }
     
     @Test
-    public void privateFieldAccess() {
+    public void privateFieldAccess() throws NoSuchFieldException, IllegalAccessException {
         Class<Student> clazz = Student.class;
         logger.debug(clazz.getName());
+        Student student = new Student();
+        Field name = clazz.getDeclaredField("name");
+        Field age = clazz.getDeclaredField("age");
+        name.setAccessible(true);
+        age.setAccessible(true);
+
+        name.set(student, "주한");
+        age.set(student, 14);
+
+        System.out.println(student.getAge() + student.getName());
     }
 }
